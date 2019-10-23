76f12295bc80fdd38574db76294d79440a1baa91
Joma Ejercito <jomaejercito@gmail.com>
2019-10-22 23:32:14 -0400

    Install react-bootstrap

--- scenter-client/src/index.js
+++ scenter-client/src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import store from './store.js'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <Provider store={store}>
