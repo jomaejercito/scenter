8159b815389db86d87af025e36e246c748369bf8
Joma Ejercito <jomaejercito@gmail.com>
2019-10-22 21:56:21 -0400

    Routes setup

--- scenter-api/config/routes.rb
+++ scenter-api/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :perfumes
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :perfumes do
+        resources :comments
+      end       
+    end
+  end
 end
